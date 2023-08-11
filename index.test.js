d4ffa4605f861f08bf23cee2ef5ab6357c440786
Valeriia Harmash <valeriaharmash@gmail.com>
2023-08-11 13:45:09 -0400

    put post delete and tests

--- index.test.js
+++ index.test.js
@@ -20,4 +20,44 @@ describe('./musicians endpoint', () => {
 		const res = await request(app).get(`/musicians/${musicianId}`)
 		expect(res.statusCode).toBe(200)
 	})
+
+	test('POST creates musician and returns it', async () => {
+		const musicianData = { name: 'John Doe', instrument: 'Piano' }
+
+		const response = await request(app)
+			.post('/musicians')
+			.send(musicianData)
+			.expect(200)
+
+		expect(response.body).toMatchObject(musicianData)
+	})
+
+	test('updates a musician and returns status 200', async () => {
+		const musician = await Musician.create({
+			name: 'Old Name',
+			instrument: 'Old Instrument'
+		})
+
+		const updatedData = { name: 'New Name', instrument: 'New Instrument' }
+
+		const response = await request(app)
+			.put(`/musicians/${musician.id}`)
+			.send(updatedData)
+			.expect(200)
+
+		expect(response.status).toBe(200)
+	})
+
+	test('deletes a musician and returns status 200', async () => {
+		const musician = await Musician.create({
+			name: 'Musician to Delete',
+			instrument: 'Instrument to Delete'
+		})
+
+		const response = await request(app)
+			.delete(`/musicians/${musician.id}`)
+			.expect(200)
+
+		expect(response.status).toBe(200)
+	})
 })
--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const { db } = require('../db/connection')
 
 const port = 3000
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.get('/musicians', async (req, res) => {
 	try {
 		const data = await Musician.findAll()
@@ -28,6 +31,59 @@ app.get('/musicians/:id', async (req, res) => {
 		res.status(404).send("Musician doesn't exist.")
 	} catch (error) {
 		console.error(error)
+	}
+})
+
+app.post('/musicians', async (req, res, next) => {
+	try {
+		const musician = await Musician.create(req.body)
+
+		if (!musician) {
+			throw new Error('No musician created')
+		}
+		res.send(musician)
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.put('/musicians/:id', async (req, res, next) => {
+	try {
+		const updated = await Musician.update(req.body, {
+			where: {
+				id: req.params.id
+			}
+		})
+
+		console.log(updated)
+
+		if (updated[0] === 0) {
+			throw new Error('No update made')
+		}
+
+		res.sendStatus(200)
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.delete('/musicians/:id', async (req, res, next) => {
+	try {
+		const deleted = await Musician.destroy({
+			where: {
+				id: req.params.id
+			}
+		})
+
+		console.log(deleted)
+
+		if (deleted === 0) {
+			throw new Error('No musician deleted')
+		}
+
+		res.sendStatus(200)
+	} catch (error) {
+		next(error)
 	}
 })
 
